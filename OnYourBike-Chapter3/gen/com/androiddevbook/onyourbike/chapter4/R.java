e5800cb7101861efa7b3af77324fa1825db0bcaa
cclamb <chrislambistan@gmail.com>
2014-02-08 14:41:10 -0700

    adding settings activity and intents

--- OnYourBike-Chapter3/gen/com/androiddevbook/onyourbike/chapter4/R.java
+++ OnYourBike-Chapter3/gen/com/androiddevbook/onyourbike/chapter4/R.java
@@ -24,16 +24,19 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080003;
-        public static final int start_button=0x7f080001;
-        public static final int stop_button=0x7f080002;
-        public static final int timer=0x7f080000;
+        public static final int action_settings=0x7f080004;
+        public static final int start_button=0x7f080002;
+        public static final int stop_button=0x7f080003;
+        public static final int timer=0x7f080001;
+        public static final int vibrate_checkbox=0x7f080000;
     }
     public static final class layout {
-        public static final int activity_timer=0x7f030000;
+        public static final int activity_settings=0x7f030000;
+        public static final int activity_timer=0x7f030001;
     }
     public static final class menu {
         public static final int main=0x7f070000;
+        public static final int settings=0x7f070001;
     }
     public static final class string {
         public static final int action_settings=0x7f050001;
@@ -41,7 +44,9 @@ public final class R {
         public static final int hello_world=0x7f050002;
         public static final int start_button=0x7f050004;
         public static final int stop_button=0x7f050005;
+        public static final int title_activity_settings=0x7f050006;
         public static final int title_activity_timer=0x7f050003;
+        public static final int vibrate_checkbox=0x7f050007;
     }
     public static final class style {
         /** 
